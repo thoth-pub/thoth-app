216ca72c47960bb327854c0a55e0ce7433bfe89d
rafanasiev-digis <r.afanasiev@digiscorp.com>
2025-10-13 15:07:48 +0300

    feat: change sidebar

--- src/features/layout/Navigation/Navigation.tsx
+++ src/features/layout/Navigation/Navigation.tsx
@@ -25,42 +25,64 @@ const Navigation = () => {
       className="sticky top-3 max-h-[40rem] shrink-0 rounded-[var(--border-nav-radius)] border-1 border-[var(--color-nav-border)] bg-[var(--color-nav-background)] p-3"
     >
       <motion.div
-        animate={{ width: isExpanded ? '15rem' : '4rem' }}
+        animate={{ width: isExpanded ? '15rem' : '2.5rem' }}
         className="flex h-full max-w-[15rem] flex-col gap-2 overflow-hidden duration-300"
       >
-        <Image
-          src="/logo.png"
-          alt="Thoth Open Metadata logo"
-          className="block min-h-[97px] min-w-[170px] shrink-0"
-          width={170}
-          height={97}
-          priority
-        />
-
+        <div className={`flex items-center justify-between gap-4 ${isExpanded ? 'flex-row' : 'flex-col'}`}>
+          {isExpanded ? (
+            <Image
+              src="/logo.png"
+              alt="Thoth Open Metadata logo"
+              className="block min-h-[97px] min-w-[170px] shrink-0"
+              width={170}
+              height={97}
+              priority
+            />
+          ) : (
+            <Image
+              src="/logo_small.png"
+              alt="Thoth Open Metadata logo"
+              className="block min-h-[42px] min-w-[40px] shrink-0"
+              width={40}
+              height={42}
+              priority
+            />
+          )}
+          <IconButton onClick={() => setIsExpanded(!isExpanded)} className={`${isExpanded && 'self-start'}`}>
+            {!isExpanded ? <ArrowForwardIosRoundedIcon /> : <ArrowBackIosNewRoundedIcon />}
+          </IconButton>
+        </div>
         <ChangeActivePublisher />
         <ContentLanguage />
         <SignOutButton />
         <nav>
           <ul className="flex flex-col rounded-[var(--border-nav-radius)] border-1 border-[var(--color-nav-border)]">
             {PAGES.map(({ name, href, icon: Icon }) => (
-              <li key={href} className="gap-2 px-[18px] py-2 duration-300 hover:bg-[var(--color-hover)]">
+              <li
+                key={href}
+                className={`py-2 duration-300 hover:bg-[var(--color-hover)] ${isExpanded ? 'px-4' : 'px-1.5'}`}
+              >
                 <Link href={href} className="flex shrink-0 items-center gap-2">
-                  <Icon color="primary" />
-                  <Typography
-                    color="primary"
-                    component="span"
-                    className={`transition-opacity duration-300 ${isExpanded ? 'opacity-100' : 'opacity-0'}`}
-                  >
-                    {name}
-                  </Typography>
+                  <Icon color="primary" className={`${!isExpanded && 'm-auto'}`} />
+                  {isExpanded && (
+                    <Typography
+                      color="primary"
+                      component="span"
+                      className={`transition-opacity duration-300 ${isExpanded ? 'opacity-100' : 'opacity-0'}`}
+                    >
+                      {name}
+                    </Typography>
+                  )}
                 </Link>
               </li>
             ))}
           </ul>
         </nav>
 
-        <div className="mt-auto flex gap-2 rounded-[var(--border-nav-radius)] border-1 border-[var(--color-nav-border)] px-[18px] py-2">
-          <PermIdentityRoundedIcon color="primary" className="my-auto" />
+        <div
+          className={`mt-auto flex gap-2 rounded-[var(--border-nav-radius)] border-1 border-[var(--color-nav-border)] py-2 ${isExpanded ? 'px-4' : 'h-[2.5rem] w-[2.5rem] px-1.5'}`}
+        >
+          <PermIdentityRoundedIcon color="primary" className="my-auto shrink-0" />
           <Typography
             color="primary"
             component="span"
@@ -72,10 +94,6 @@ const Navigation = () => {
             </Typography>
           </Typography>
         </div>
-
-        <IconButton onClick={() => setIsExpanded(!isExpanded)} className="mx-auto">
-          {isExpanded ? <ArrowForwardIosRoundedIcon /> : <ArrowBackIosNewRoundedIcon />}
-        </IconButton>
       </motion.div>
     </Paper>
   );
