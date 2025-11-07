8ab6d52a984e88bf0d970c3460f4f2a8bbcc2a22
rafanasiev-digis <r.afanasiev@digiscorp.com>
2025-11-07 08:02:24 +0200

    feat: update sign out logic

--- src/features/auth/ui/SignOutButton/SignOutButton.tsx
+++ src/features/auth/ui/SignOutButton/SignOutButton.tsx
@@ -5,14 +5,23 @@ import LogoutIcon from '@mui/icons-material/Logout';
 import { signOutAction } from '@/app/actions';
 import usePublisherStateMachine from '@/src/entities/publisher/store/hooks/usePublisherStateMachine';
 import { Button } from '@/src/shared/ui';
+import { useApolloClient } from '@apollo/client/react';
+import { useEffect } from 'react';
 
 const SignOutButton = () => {
   const { resetLinkedPublishers } = usePublisherStateMachine();
+  const client = useApolloClient();
 
   const signOut = async () => {
     await signOutAction();
-    resetLinkedPublishers();
   };
+
+  useEffect(() => {
+    return () => {
+      resetLinkedPublishers();
+      client.clearStore();
+    };
+  }, []);
 
   return (
     <form action={signOut}>
