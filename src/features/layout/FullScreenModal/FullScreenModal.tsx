35bd32c2a26ce11f8d0e37f386b5ad146477585c
rafanasiev-digis <r.afanasiev@digiscorp.com>
2025-12-30 15:23:42 +0200

    fix: styles

--- src/features/layout/FullScreenModal/FullScreenModal.tsx
+++ src/features/layout/FullScreenModal/FullScreenModal.tsx
@@ -33,7 +33,7 @@ const FullScreenModal = (props: FullScreenModalProps) => {
       <div className="relative h-dvh w-dvw overflow-auto bg-(--color-modal-content-background) px-8 py-2 lg:px-5 lg:py-3">
         <div className="m-auto flex h-full flex-row">
           <Navigation linkedPublishers={publishers} isSuperAdmin={isAdmin} />
-          <div className="flex grow flex-col gap-(--default-gap) px-8 py-[12px]">
+          <div className="scrollbar-hidden flex grow flex-col gap-(--default-gap) overflow-x-auto px-8 py-[12px]">
             <ContentSection>
               <div className="flex justify-between">
                 <Typography variant="h1" component="h3" className="pl-4 text-(--color-typography)">
