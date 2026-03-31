0d202b037ec4bb103a5c4768168a2309756636b2
rafanasiev-digis <r.afanasiev@digiscorp.com>
2026-03-31 12:45:26 +0300

    feat: added favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,15 @@ const economica = Economica({
 export const metadata: Metadata = {
   title,
   description,
+  icons: {
+    icon: [
+      { url: '/favicons/favicon.ico', sizes: 'any' },
+      { url: '/favicons/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicons/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [{ url: '/favicons/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }],
+  },
+  manifest: '/favicons/site.webmanifest',
 };
 
 export default async function RootLayout({
