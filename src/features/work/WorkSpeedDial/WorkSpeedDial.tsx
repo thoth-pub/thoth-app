32e3247f40825543251d357406b4a59f46660409
rafanasiev-digis <r.afanasiev@digiscorp.com>
2026-03-31 12:56:46 +0300

    feat: hide recommendations sections

--- src/features/work/WorkSpeedDial/WorkSpeedDial.tsx
+++ src/features/work/WorkSpeedDial/WorkSpeedDial.tsx
@@ -14,30 +14,30 @@ import {
   useCreateNewWorkEdition,
   useCreateWorkTranslation,
   useWork,
-  useWorkRecommendations,
+  // useWorkRecommendations,
   useWorkSet,
 } from '@/src/entities/work';
 import useDeleteWork from '@/src/entities/work/api/hooks/useDeleteWork';
 import type { WorkId } from '@/src/entities/work/model/work.types';
-import { ANCHORS } from '@/src/shared/constants';
-import { SpeedDial, SpeedDialActions, TranslatedContent, Typography } from '@/src/shared/ui';
-import DataIndicator from '@/src/shared/ui/core/DataIndicator/DataIndicator';
+// import { ANCHORS } from '@/src/shared/constants';
+import { SpeedDial, SpeedDialActions, TranslatedContent } from '@/src/shared/ui';
 
+// import DataIndicator from '@/src/shared/ui/core/DataIndicator/DataIndicator';
 import { AddVolume } from './components/AddVolume';
 
 type WorkSpeedDialProps = {
   workId: WorkId;
 };
 
-const buttonItemStyle = {
-  minWidth: '10px',
-  minHeight: '10px',
-  height: '10px',
-  width: '10px',
-  boxShadow: 'none !important',
-};
+// const buttonItemStyle = {
+//   minWidth: '10px',
+//   minHeight: '10px',
+//   height: '10px',
+//   width: '10px',
+//   boxShadow: 'none !important',
+// };
 
-const { BASIC_DETAILS, DESCRIPTIONS, CONTRIBUTIONS, FUNDINGS } = ANCHORS;
+// const { BASIC_DETAILS, DESCRIPTIONS, CONTRIBUTIONS, FUNDINGS } = ANCHORS;
 
 const WorkSpeedDial = (props: WorkSpeedDialProps) => {
   const { workId } = props;
@@ -45,18 +45,18 @@ const WorkSpeedDial = (props: WorkSpeedDialProps) => {
   const [openAddVolume, setOpenAddVolume] = useState(false);
   const [openMetaDialog, setOpenMetaDialog] = useState(false);
 
-  const {
-    isAllInformationFilled,
-    isEmpty,
-    isBasicDetailsSectionEmpty,
-    isBasicDetailsSectionFilled,
-    isDescriptionsSectionEmpty,
-    isDescriptionsSectionFilled,
-    isContributionsEmpty,
-    isContributionsRequired,
-    isFundingsEmpty,
-    isFundingsRequired,
-  } = useWorkRecommendations({ workId });
+  // const {
+  //   isAllInformationFilled,
+  //   isEmpty,
+  //   isBasicDetailsSectionEmpty,
+  //   isBasicDetailsSectionFilled,
+  //   isDescriptionsSectionEmpty,
+  //   isDescriptionsSectionFilled,
+  //   isContributionsEmpty,
+  //   isContributionsRequired,
+  //   isFundingsEmpty,
+  //   isFundingsRequired,
+  // } = useWorkRecommendations({ workId });
   const { work } = useWork(workId);
   const { workSet } = useWorkSet(workId);
   const { createNewWorkEdition, loading: isEditionPending } = useCreateNewWorkEdition();
@@ -91,20 +91,25 @@ const WorkSpeedDial = (props: WorkSpeedDialProps) => {
 
   const actions = [
     {
-      icon: <SaveAltIcon color={isActionPending ? 'disabled' : 'primary'} onClick={() => !isActionPending && setOpenMetaDialog(true)} />,
-      name: 'metadata',
-    },
-    {
       icon: (
-        <DataIndicator
-          isEmpty={isEmpty}
-          isValid={isAllInformationFilled}
-          sx={{ boxShadow: 'none !important' }}
-          component="span"
+        <SaveAltIcon
+          color={isActionPending ? 'disabled' : 'primary'}
+          onClick={() => !isActionPending && setOpenMetaDialog(true)}
         />
       ),
-      name: 'Recommendations',
+      name: 'metadata',
     },
+    // {
+    //   icon: (
+    //     <DataIndicator
+    //       isEmpty={isEmpty}
+    //       isValid={isAllInformationFilled}
+    //       sx={{ boxShadow: 'none !important' }}
+    //       component="span"
+    //     />
+    //   ),
+    //   name: 'Recommendations',
+    // },
     {
       icon: <DeleteOutlineIcon color={isActionPending ? 'disabled' : 'primary'} onClick={onDeleteWork} />,
       name: 'delete',
@@ -143,56 +148,59 @@ const WorkSpeedDial = (props: WorkSpeedDialProps) => {
           <SpeedDialActions
             key={action.name}
             icon={action.icon}
+            // slotProps={{
+            //   tooltip:
+            //     action.name === 'Recommendations'
+            //       ? {
+            //           title: (
+            //             <ul className="flex flex-col gap-2 p-0 text-black">
+            //               <Typography variant="body2" component="li">
+            //                 <a href={`#${BASIC_DETAILS}`} className="capitalize">
+            //                   <DataIndicator
+            //                     isEmpty={isBasicDetailsSectionEmpty}
+            //                     isValid={isBasicDetailsSectionFilled}
+            //                     sx={{ ...buttonItemStyle }}
+            //                   />
+            //                   <TranslatedContent content="core details" />
+            //                 </a>
+            //               </Typography>
+            //               <Typography variant="body2" component="li">
+            //                 <a href={`#${CONTRIBUTIONS}`}>
+            //                   <DataIndicator
+            //                     isEmpty={isContributionsEmpty}
+            //                     isValid={!isContributionsRequired}
+            //                     sx={{ ...buttonItemStyle }}
+            //                   />
+            //                   <TranslatedContent content="contributions" />
+            //                 </a>
+            //               </Typography>
+            //               <Typography variant="body2" component="li">
+            //                 <a href={`#${DESCRIPTIONS}`}>
+            //                   <DataIndicator
+            //                     isEmpty={isDescriptionsSectionEmpty}
+            //                     isValid={isDescriptionsSectionFilled}
+            //                     sx={{ ...buttonItemStyle }}
+            //                   />
+            //                   <TranslatedContent content="descriptions" />
+            //                 </a>
+            //               </Typography>
+            //               <Typography variant="body2" component="li">
+            //                 <a href={`#${FUNDINGS}`}>
+            //                   <DataIndicator
+            //                     isEmpty={isFundingsEmpty}
+            //                     isValid={!isFundingsRequired}
+            //                     sx={{ ...buttonItemStyle }}
+            //                   />
+            //                   <TranslatedContent content="funding" />
+            //                 </a>
+            //               </Typography>
+            //             </ul>
+            //           ),
+            //         }
+            //       : { open: true, title: <TranslatedContent content={action.name} /> },
+            // }}
             slotProps={{
-              tooltip:
-                action.name === 'Recommendations'
-                  ? {
-                      title: (
-                        <ul className="flex flex-col gap-2 p-0 text-black">
-                          <Typography variant="body2" component="li">
-                            <a href={`#${BASIC_DETAILS}`} className="capitalize">
-                              <DataIndicator
-                                isEmpty={isBasicDetailsSectionEmpty}
-                                isValid={isBasicDetailsSectionFilled}
-                                sx={{ ...buttonItemStyle }}
-                              />
-                              <TranslatedContent content="core details" />
-                            </a>
-                          </Typography>
-                          <Typography variant="body2" component="li">
-                            <a href={`#${CONTRIBUTIONS}`}>
-                              <DataIndicator
-                                isEmpty={isContributionsEmpty}
-                                isValid={!isContributionsRequired}
-                                sx={{ ...buttonItemStyle }}
-                              />
-                              <TranslatedContent content="contributions" />
-                            </a>
-                          </Typography>
-                          <Typography variant="body2" component="li">
-                            <a href={`#${DESCRIPTIONS}`}>
-                              <DataIndicator
-                                isEmpty={isDescriptionsSectionEmpty}
-                                isValid={isDescriptionsSectionFilled}
-                                sx={{ ...buttonItemStyle }}
-                              />
-                              <TranslatedContent content="descriptions" />
-                            </a>
-                          </Typography>
-                          <Typography variant="body2" component="li">
-                            <a href={`#${FUNDINGS}`}>
-                              <DataIndicator
-                                isEmpty={isFundingsEmpty}
-                                isValid={!isFundingsRequired}
-                                sx={{ ...buttonItemStyle }}
-                              />
-                              <TranslatedContent content="funding" />
-                            </a>
-                          </Typography>
-                        </ul>
-                      ),
-                    }
-                  : { open: true, title: <TranslatedContent content={action.name} /> },
+              tooltip: { open: true, title: <TranslatedContent content={action.name} /> },
             }}
           />
         ))}
--- src/shared/ui/layout/RecommendedSection/RecommendedSection.tsx
+++ src/shared/ui/layout/RecommendedSection/RecommendedSection.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { useState } from 'react';
+// import { useState } from 'react';
 
-import DataIndicator from '../../core/DataIndicator/DataIndicator';
+// import DataIndicator from '../../core/DataIndicator/DataIndicator';
 import ContentSection from '../ContentSection/ContentSection';
 
 type RecommendedSectionProps = {
@@ -15,29 +15,29 @@ type RecommendedSectionProps = {
 };
 
 const RecommendedSection = (props: RecommendedSectionProps) => {
-  const { title, isEmpty, isValid, id, className, children } = props;
+  const { title, id, className, children } = props;
 
-  const [showRecommendations, setShowRecommendations] = useState(false);
+  // const [showRecommendations, setShowRecommendations] = useState(false);
 
-  const handleRecommendations = () => {
-    setShowRecommendations((prev) => !prev);
-  };
+  // const handleRecommendations = () => {
+  //   setShowRecommendations((prev) => !prev);
+  // };
 
   return (
     <ContentSection
       title={title}
       id={id}
       className={className}
-      headerContent={
-        <DataIndicator
-          isActive={showRecommendations}
-          isEmpty={isEmpty}
-          isValid={isValid}
-          onClick={handleRecommendations}
-        />
-      }
+      // headerContent={
+      //   <DataIndicator
+      //     isActive={showRecommendations}
+      //     isEmpty={isEmpty}
+      //     isValid={isValid}
+      //     onClick={handleRecommendations}
+      //   />
+      // }
     >
-      {children && children({ showRecommendations })}
+      {children && children({ showRecommendations: false })}
     </ContentSection>
   );
 };
