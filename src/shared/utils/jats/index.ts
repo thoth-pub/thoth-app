45df1cc72d6fbb82e2ef9166df978bb290b13b2c
rafanasiev-digis <r.afanasiev@digiscorp.com>
2026-04-24 13:34:08 +0300

    feat: update preview logic for markdown

--- src/shared/utils/jats/index.ts
+++ src/shared/utils/jats/index.ts
@@ -1,17 +1,17 @@
 
 export const jatsToHtml = (jats: string) => {
   return jats
-    .replace('<bold>', '<b>')
-    .replace('</bold>', '</b>')
-    .replace('<italic>', '<i>')
-    .replace('</italic>', '</i>')
-    .replace('<strike>', '<s>')
-    .replace('</strike>', '</s>')
-    .replace('<underline>', '<u>')
-    .replace('</underline>', '</u>')
-    .replace('<link>', '<a>')
-    .replace('</link>', '</a>')
-    .replace('<ext-link>', '<a>')
-    .replace('</ext-link>', '</a>')
-    .replace('ext-link-type="uri" xlink:', '')
+    .replaceAll('<bold>', '<b>')
+    .replaceAll('</bold>', '</b>')
+    .replaceAll('<italic>', '<i>')
+    .replaceAll('</italic>', '</i>')
+    .replaceAll('<strike>', '<s>')
+    .replaceAll('</strike>', '</s>')
+    .replaceAll('<underline>', '<u>')
+    .replaceAll('</underline>', '</u>')
+    .replaceAll('<link>', '<a>')
+    .replaceAll('</link>', '</a>')
+    .replaceAll('<ext-link>', '<a>')
+    .replaceAll('</ext-link>', '</a>')
+    .replaceAll('ext-link-type="uri" xlink:', '');
 };
